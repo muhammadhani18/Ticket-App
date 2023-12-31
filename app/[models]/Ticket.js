174544c9b61878642a08e911cf07d5314e6aee82
mohammadhani18 <mhani18403@gmail.com>
2023-12-31 15:58:22 +0500

    Final app

--- app/[models]/Ticket.js
+++ app/[models]/Ticket.js
@@ -1,24 +1,25 @@
-import mongoose, {Schema} from 'mongoose';
-
+import mongoose, { Schema } from "mongoose";
 
 mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
 
-const ticketSchema = new Schema (
-    {
-        title: String,
-        description: String,
-        category: String,
-        priority: Number,
-        progress: Number,
-        status: String,
-        active: Boolean
-    },
-    {
-        timestamps: true,
-    }
-)
+console.log("Connected");
 
-const Ticket = mongoose.models.Ticket || mongoose.models("Ticket", ticketSchema)
+const ticketSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    category: String,
+    priority: Number,
+    progress: Number,
+    status: String,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Ticket = mongoose.models.Ticket || mongoose.model("Ticket", ticketSchema);
 
 export default Ticket;
--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -1,4 +1,4 @@
-import Ticket from "../[models]/Ticket";
+import Ticket from "../../[models]/Ticket";
 import {NextResponse} from 'next/server';
 
 
@@ -7,7 +7,7 @@ export async function POST(req) {
     try {
         const body = await req.json();
         const ticketData = body.formData
-        await Ticket.creat(ticketData)
+        await Ticket.create(ticketData)
         
         return NextResponse.json({message: "Ticket Created"}, {status: 201})
 
@@ -17,3 +17,15 @@ export async function POST(req) {
 }
 
 
+export async function GET() {
+    try {
+        const tickets = await Ticket.find();
+        
+        return NextResponse.json({tickets}, {status: 200});
+
+    } 
+    catch (error) 
+    {
+        return NextResponse.json({message: "Error", error}, {status: 500})       
+    }
+}
