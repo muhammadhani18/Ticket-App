a0ad23bc9e8187a3413d926c517ecb567a449280
mohammadhani18 <mhani18403@gmail.com>
2023-12-31 13:22:10 +0500

    Styling and Pages added

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import TicketCard from './[components]/TicketCard';
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='p-5 '>
+      <div className='lg:grid grid-cols-2 xl:grid-cols-4'>
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
+    </div>
   )
 }
 
